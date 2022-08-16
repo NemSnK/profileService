38752140284a4e2bbce1ec3a97e53d19a29aa8f8
Zhassulan Kaitanov <kaitanov.work@gmail.com>
2022-08-16 20:50:23 +0600

    init

---
+++ src/main/java/kata/academy/eurekaprofileservice/EurekaProfileService.java
@@ -0,0 +1,14 @@
+package kata.academy.eurekaprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaProfileService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProfileService.class, args);
+    }
+}
