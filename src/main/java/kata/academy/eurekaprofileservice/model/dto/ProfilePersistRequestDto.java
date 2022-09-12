6be0c388cc1283a8861d8072ff431ba317bed795
Zhassulan Kaitanov <kaitanov.work@gmail.com>
2022-09-12 18:17:01 +0600

    fix

--- src/main/java/kata/academy/eurekaprofileservice/model/dto/ProfilePersistRequestDto.java
+++ src/main/java/kata/academy/eurekaprofileservice/model/dto/ProfilePersistRequestDto.java
@@ -9,8 +9,8 @@ import java.time.LocalDate;
 public record ProfilePersistRequestDto(
         String firstName,
         String lastName,
-        @DateTimeFormat(pattern="yyyy-MM-dd")
-        @JsonFormat(pattern="yyyy-MM-dd")
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate birthdate,
         Gender gender) {
 }
