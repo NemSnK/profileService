69dce3fe00601ddb875614662c69a5b79b50445c
Zhassulan Kaitanov <kaitanov.work@gmail.com>
2022-09-12 16:25:25 +0600

    fix

--- src/main/java/kata/academy/eurekaprofileservice/EurekaProfileService.java
+++ src/main/java/kata/academy/eurekaprofileservice/EurekaProfileService.java
@@ -3,9 +3,11 @@ package kata.academy.eurekaprofileservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class EurekaProfileService {
 
     public static void main(String[] args) {
